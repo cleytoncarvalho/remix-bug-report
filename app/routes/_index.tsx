75c1094bdc51ce28599f56f050da9e2b6001b23c
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2024-07-05 09:28:55 -0300

    bug report

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+import { useEffect, useRef } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,41 +11,23 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  useEffect(() => {
+    console.log(formRef)
+  }, [])
+
   return (
     <div className="font-sans p-4">
+
       <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+      <br />
+      <br />
+
+      <Form ref={formRef}>
+        <input type="email" name="email" defaultValue="test@test.com" className="border border-gray-300" />
+      </Form>
     </div>
   );
 }
